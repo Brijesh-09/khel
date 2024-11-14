dd4c86b3a8ac113a751c457479d37c410ba1a354
unknown <brijeshkori@everymedia.world>
2024-11-14 18:53:42 +0530

    Modal_added

--- khel_frontend/src/components/Navbar.jsx
+++ khel_frontend/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';          // Changed import
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../App.css"
 
 function Navbar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleClose = () => setIsModalOpen(false);
+  const handleShow = () => setIsModalOpen(true);
+
   return (
-    <nav className="navbar">
+    <><nav className="navbar">
       <div className="navbar-logo">
         <img src="path-to-your-logo" alt="Logo" />
       </div>
@@ -14,9 +22,33 @@ function Navbar() {
         {/* <a href="/contact">Contact</a> */}
       </div>
       <div className="navbar-actions">
-        <button className="action-btn primary">Join Now</button>
+        <button className="action-btn primary" onClick={handleShow}>Join Now</button>
       </div>
     </nav>
+    
+    <Modal show={isModalOpen} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+          <input 
+            type="text"
+            placeholder="Type something..." 
+          />
+          <button type="submit">Submit</button>
+        </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
---
+++ khel_frontend/src/components/join.jsx
@@ -0,0 +1,13 @@
+// src/pages/JoinPage.js
+import React from 'react';
+
+function JoinPage() {
+  return (
+    <div>
+      <h1 className='join'>Join Now Page</h1>
+      {/* Add your content for the "Join Now" page here */}
+    </div>
+  );
+}
+
+export default JoinPage;
